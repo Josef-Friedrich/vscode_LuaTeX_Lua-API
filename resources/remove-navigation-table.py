14ec05c27c6706de33b3387c0ee52dfedae38cd1
Josef Friedrich <josef@friedrich.rocks>
2023-02-25 12:13:35 +0100

    Add script to remove the navigation table

---
+++ resources/remove-navigation-table.py
@@ -0,0 +1,27 @@
+#! /usr/bin/python
+
+import re
+import glob
+
+from pathlib import Path
+
+
+def patch_file(file_name: str):
+    content: str = ""
+
+    with open(file_name) as src:
+        content = src.read()
+
+    content = re.sub(r"\n_N\..*\n", r"\n", content)
+
+    with open(file_name, "w") as dest:
+        dest.write(content)
+
+    print(content)
+
+
+for file_name in glob.glob(
+    str(Path(__file__).resolve().parent.parent) + "/library/**/*.lua"
+):
+    print(file_name)
+    patch_file(file_name)
